83ae1b24ea8926fdf96e32351c59836588e5fb2d
Shahariar Aryan <shahariarahmed834@gmail.com>
2021-02-07 12:26:42 +0600

    LabTaskNo3

---
+++ Lab Task/Lab Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers :");
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    console.Write(i+" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
