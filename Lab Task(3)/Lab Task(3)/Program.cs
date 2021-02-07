b6ded7904637827425988f1a597bec363c2672a5
Shahariar Aryan <shahariarahmed834@gmail.com>
2021-02-07 14:00:02 +0600

    Main Lab Task

---
+++ Lab Task(3)/Lab Task(3)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Student
+{
+    public String name;
+    public int  id;
+    public String depertment;
+    public float cgpa;
+
+    public void ShowDetails()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("ID " + id);
+        Console.WriteLine("Depertment" + depertment);
+        Console.WriteLine("CGPA " + cgpa);
+
+    }
+}
+class TestStudent
+{
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.name = "Fozlur Rahman";
+        s1.id = 18390973;
+        s1.depertment = "CSE";
+        s1.cgpa = 3.39f;
+        Console.WriteLine(s1.id);
+        Console.WriteLine(s1.name);
+        Console.WriteLine(s1.depertment);
+        Console.WriteLine(s1.cgpa);
+
+    }
+} 
---
+++ Lab Task(3)/TriangleClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Triangle
+{
+    public int x;
+    public int y;
+    public int z;
+     
+    {
+    public void ShowDetails()
+    {
+        Console.WriteLine("X: " + x);
+        Console.WriteLine("Y " + y);
+        Console.WriteLine("Z" + z);
+    }
+
+}
