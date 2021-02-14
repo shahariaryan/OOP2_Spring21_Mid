9fe169a4e9a9401099b5aca4a99d24b6c332ef22
Shahariar Aryan <shahariarahmed834@gmail.com>
2021-02-14 14:15:07 +0600

    task4

---
+++ labTask4/Labtask4problem2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5,3,6,8,9,6,8,6,4 };
+            foreach (int i in arr.Distinct())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
---
+++ labTask4/labTask4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Main Array is: ");
+            int[] arr1 = new int[] { 2, 4 };
+            int[] arr2 = new int[2];
+            foreach (int value in arr1)
+            {
+                Console.WriteLine(value);
+            }
+
+            Array.Copy(arr1, arr2, 2);
+
+            Console.WriteLine("Copy Array is: ");
+            foreach (int value in arr2)
+            {
+                Console.WriteLine(value);
+            }
+
+            int[] arr = { 2, 3, 4, 6, 7 };
+            int count = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length - 1; j++)
+                {
+
+                    if (arr[j] == arr[j + 1])
+                        count = count + 1;
+                }
+                
+            }
+        }
+    }
+}
